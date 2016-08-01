a06b7ec7ed33ecdc56710e56d9ce448145190fc2
langsmith <langstonlmcs@gmail.com>
2016-08-01 13:11:02 -0700

    Added menu items

--- MyApplication/app/src/main/java/com/test/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/test/myapplication/MainActivity.java
@@ -80,17 +80,29 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_trending) {
 
-        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_followed) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_more_topics) {
+
+        } else if (id == R.id.nav_business) {
+
+        } else if (id == R.id.nav_entertainment) {
+
+        } else if (id == R.id.nav_health) {
+
+        } else if (id == R.id.nav_politics) {
+
+        } else if (id == R.id.nav_scienceandtech) {
+
+        } else if (id == R.id.nav_sports) {
+
+        } else if (id == R.id.nav_US_and_UK) {
+
+        } else if (id == R.id.nav_world) {
 
         }
 
