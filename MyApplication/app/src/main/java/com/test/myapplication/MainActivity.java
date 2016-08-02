218ff999813a8a3d1e36611eeaa6a2ad17a6de6d
langsmith <langstonlmcs@gmail.com>
2016-08-01 22:13:27 -0700

    Set up job in OnCreate of MainActivity

--- MyApplication/app/src/main/java/com/test/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/test/myapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package com.test.myapplication;
 
+import android.annotation.TargetApi;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,17 +20,26 @@ import android.view.MenuItem;
 //import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setUpDrawersandView();
+
+        setUpBreakNewsCheckJob();
+
+    }
+
+    private void setUpDrawersandView() {
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -34,6 +48,27 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+    }
+
+    @TargetApi(21)
+    private void setUpBreakNewsCheckJob() {
+
+        JobInfo breakingNewsJob = new JobInfo.Builder(1,
+                new ComponentName(getPackageName(),
+                        CheckForBreakingNewsJob.class.getName()))
+                .setPeriodic(3600000) //<– Check for breaking news every hour
+                .build();
+
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        int result = jobScheduler.schedule(breakingNewsJob);
+        if (result <= 0) {
+            //stuff went wrong
+            Log.i(TAG, "setUpBreakNewsCheckJob: Error with breaking news job check");
+        }
+
+
     }
 
     @Override
@@ -75,7 +110,6 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_trending) {
-
 
 
         } else if (id == R.id.nav_followed) {
